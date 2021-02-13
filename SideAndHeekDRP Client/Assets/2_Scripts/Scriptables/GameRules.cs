8d9494d6d87ce793be461515b2e31f26298acbf1
JhonCarp <john.carpenter@twinkl.co.uk>
2021-02-13 11:13:50 +0000

    Switched to DRP version
    SWEAT

---
+++ SideAndHeekDRP Client/Assets/2_Scripts/Scriptables/GameRules.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameRules", menuName = "Game/GameRules")]
+public class GameRules : ScriptableObject
+{
+    public int id;
+
+}
