229a63b27ef9a880d19a5c9109054d7fd21312ce
John Carpenter <jbcarpenter28.acc@gmail.com>
2023-07-05 09:28:01 +0100

    Fix Colours resetting and not loading correctly

--- SideAndHeek Client/Assets/2_Scripts/Client/UI/BasePanels/PauseUI.cs
+++ SideAndHeek Client/Assets/2_Scripts/Client/UI/BasePanels/PauseUI.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PauseUI : UIPanel
 {
     [SerializeField] private Button relaunchServerButton;
+    [SerializeField] private TMP_Text disconnectButtonText;
 
+    private string disconnect = "Disconnect";
+    private string closeServer = "Close Server";
     public override void EnablePanel()
     {
         base.EnablePanel();
 
         relaunchServerButton.gameObject.SetActive(Player.LocalPlayer.isHost);
+        disconnectButtonText.text = NetworkManager.NetworkType == NetworkType.Client ? disconnect : closeServer;
     }
 
     public void OnRelaunchServerButtonPressed()
